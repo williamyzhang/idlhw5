aa101a1ab8460aec2b251e682bd643efce39c949
William Yuntai Zhang <wyz2@andrew.cmu.edu>
2025-10-24 16:47:12 -0400

    added basic things to ddpm (no cfg or vae)

--- pipelines/ddpm.py
+++ pipelines/ddpm.py
@@ -87,10 +87,10 @@ class DDPMPipeline:
             uncond_embeds = None 
         
         # TODO: starts with random noise
-        image = None # randn_tensor(image_shape, generator=generator, device=device)
+        image = randn_tensor(image_shape, generator=generator, device=device)
 
         # TODO: set step values using set_timesteps of scheduler
-        self.scheduler = None
+        self.scheduler.set_timesteps(num_inference_steps=num_inference_steps, device=device)
         
         # TODO: inverse diffusion process with for loop
         for t in self.progress_bar(self.scheduler.timesteps):
@@ -101,12 +101,12 @@ class DDPMPipeline:
                 model_input = None 
                 c = None 
             else:
-                model_input = None 
+                model_input = image
                 # NOTE: leave c as None if you are not using CFG
                 c = None
             
             # TODO: 1. predict noise model_output
-            model_output = None
+            model_output = self.unet(model_input, t, c)
             
             if guidance_scale is not None or guidance_scale != 1.0:
                 # TODO: implement cfg
@@ -114,7 +114,7 @@ class DDPMPipeline:
                 model_output = None
             
             # TODO: 2. compute previous image: x_t -> x_t-1 using scheduler
-            image = None 
+            image = self.scheduler.step(model_output, t, image) 
             
         
         # NOTE: this is for latent DDPM
@@ -126,7 +126,7 @@ class DDPMPipeline:
             image = None 
         
         # TODO: return final image, re-scale to [0, 1]
-        image = None 
+        image = (image / 2 + 0.5).clamp(0, 1) 
         
         # convert to PIL images
         image = image.cpu().permute(0, 2, 3, 1).numpy()
